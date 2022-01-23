6eb83210b6194db996da1aaba5fb65754d09afad
Callum McIntyre <callum@pointer.gg>
2022-01-23 21:31:11 +0000

    Add title element

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,21 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
 import PyodideProvider from '../components/pyodide-provider'
 import { Toaster } from 'react-hot-toast'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PyodideProvider>
-      <Toaster />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </PyodideProvider>
+    <>
+      <Head>
+        <title>Pyground</title>
+      </Head>
+      <PyodideProvider>
+        <Toaster />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PyodideProvider>
+    </>
   )
 }
 
